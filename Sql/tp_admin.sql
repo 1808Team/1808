565445c8e8977aff47b5216f04cba0d47118c4ec
lj1808 <2934095009@qq.com>
2020-09-14 08:37:01 +0800

    jj

--- Sql/tp_admin.sql
+++ Sql/tp_admin.sql
@@ -1,32 +1,36 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : shop
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : milletdata
+Source Server         : localhost_3306
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : milletdata
 
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
 
- Date: 11/09/2020 14:35:25
+Date: 2020-09-14 08:36:20
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for tp_admin
 -- ----------------------------
 DROP TABLE IF EXISTS `tp_admin`;
-CREATE TABLE `tp_admin`  (
+CREATE TABLE `tp_admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `root` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账号',
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
-  `phone` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `root` varchar(255) DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `phone` varchar(200) DEFAULT NULL COMMENT '手机号',
+  `Personal_signature` varchar(255) DEFAULT NULL COMMENT '个性签名',
+  `My_hobbies` varchar(255) DEFAULT NULL COMMENT '我的爱好',
+  `Shipping_address` varchar(255) DEFAULT NULL COMMENT '收货地址',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of tp_admin
+-- ----------------------------
+INSERT INTO `tp_admin` VALUES ('5', 'admin', '123', '12345678900', '大幅度发', '11啥事', '大傻傻的');
